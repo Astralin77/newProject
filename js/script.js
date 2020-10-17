bef1e58d7f93e97bd013481b39f5013344e4d605
Andrey <exemple@gmail.com>
2020-10-17 11:18:10 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// Интерполяция
+
+// const category = 'toys';
+// const user = "Andrey";
+
+// console.log(`Hello, ${user}`);
+
+// console.log(`http://someurl.com/${category}/5`);
+console.log(2 + 2 * 2 != 8);
+
+const isChecked = true,
+      isClose = false;
+
+ console.log(isChecked && !isClose);     
